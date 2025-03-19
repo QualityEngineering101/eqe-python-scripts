3c42ff99ba1682809131741c0e345094dc2e34eb
DeanMarshEQE <dean.f.marsh@gmail.com>
2025-03-18 22:11:22 -0400

    Added Product Test Cases

--- fastAPI/tests/test_products.py
+++ fastAPI/tests/test_products.py
@@ -18,23 +18,38 @@ def test_get_product_by_id(test_client:TestClient):
         assert response.status_code == 404
 
 @pytest.mark.parametrize(
-    "payload, expected_status",
+    "payload, expected_status_code",
     [
         ({"name": "Product D", "description": "Product D Description", "status": "draft"}, 201),
         ({"name": "Product E", "description": "Product E Description", "status": "active"}, 201),
-        ({"name": "Product F", "description": "Product F Description", "status": "archived"}, 201), 
+        ({"name": "Product F", "description": "Product F Description", "status": "archived"}, 201),
+        ({"description": "Missing Product Name", "status":"draft"},422),
+        ({"name": "Required Field Check", "description":"Invalid Status (Unsupported)", "status":"approved"}, 422),
+        ({"name": "Required Field Check", "description":"Invalid Status (Upper)","status":"DRAFT"},422),
+        ({"name": "Required Field Check", "description":"Invalid Status (Title)","status":"Draft"},422),
+        ({"name": "Required Field Check", "description":"Status is Blank","status":""},422),
+        ({"name": "Required Field Check", "description":"Status is Space","status":" "},422),
+        ({"name": "Required Field Check", "description":"Status is Missing"},201),
+        ({"name": "Required Field Check", "description":"Unsupported Field", "status":"draft","department":"marketing"},201)
     ],
 )
-def test_create_product(test_client:TestClient, payload, expected_status):
+def test_create_product(test_client:TestClient, payload, expected_status_code):
     response = test_client.post("/products/",json=payload)
-    assert response.status_code == expected_status
+    assert response.status_code == expected_status_code
     data = response.json()
-    assert payload["name"] == data["name"]
-    assert payload["description"] == data["description"]
-    assert payload["status"] == data["status"]
-    assert "id" in data
-    assert "created_at" in data
-
+    try:
+        if response.status_code == 201:
+            assert payload["name"] == data["name"]
+            assert payload["description"] == data["description"]
+            assert payload["status"] == data["status"] or "draft"
+            assert "id" in data
+            assert "created_at" in data
+        else:
+            assert "detail" in data
+            print(f"Validational error: {data}")
+    except Exception as e:
+        print(f"Unexpected Exception: {e}")
+        
 def test_invalid_product_fetch(test_client: TestClient):
     """ Test getting a non-existent product """
     response = test_client.get("/product/9999")
